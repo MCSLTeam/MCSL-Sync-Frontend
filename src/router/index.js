af3afd4242d17d808d8eb928eb89f44cf5813d56
daizihan233 <y066ydg5@duck.com>
2024-04-05 00:20:25 +0800

    feat(Router): 添加主页的路由

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import {createRouter, createWebHistory} from "vue-router";
+
+const router = createRouter(
+    {
+        history: createWebHistory(),
+        routes: [
+            {
+                path: '/',
+                name: 'Home',
+                component: () => import("../views/Home.vue")
+            }
+        ]
+    }
+)
+
+export default router
