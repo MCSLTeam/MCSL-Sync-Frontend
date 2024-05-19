0207fc883fce76371e0fd6ca7d7b3b41044b7ce9
Tigercrl <tigercrl@icloud.com>
2024-05-19 13:29:01 +0800

    Init | 准备完全重写

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MCSL-Sync-Frontend
-MCSL-Sync 使用 Vue3 & NaiveUI 开发的前端
+## 简介
+MCSL-Sync | MC服务器核心镜像同步工具 | 前端
+
+## 快速开始
+```shell
+pnpm install
+pnpm dev # 开发
+pnpm build # 构建
+```
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <n-split direction="horizontal" default-size=0.17>
-    <template #1>
-      <Menu />
-    </template>
-    <template #2>
-      <n-split direction="vertical" min=0.92 max=0.92 default-size=0.92>
-        <template #1>
-          <router-view />
-        </template>
-        <template #2>
-          <Footer />
-        </template>
-      </n-split>
-    </template>
-  </n-split>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script setup>
-import { NSplit } from "naive-ui";
-import { RouterView } from "vue-router";
-import Menu from "./components/Menu.vue";
-import Footer from "./components/Footer.vue";
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
