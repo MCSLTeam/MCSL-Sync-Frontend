f32a09c47d77183723ca7e6ad7d848eb1f2bc875
LxHTT <lxhtt@mcsl.com.cn>
2024-05-26 14:33:07 +0800

    feat: seo

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
             name: "cores",
             component: Cores,
             meta: {
-                title: "核心列表 | MCSL Sync"
+                title: "核心列表 | MCSL-Sync 服务端镜像站 | MCSL, 我的世界核心, 我的世界服务器, 我的世界, MC 服务器, Minecraft 服务器, 我的世界开服器, MC 开服软件"
             }
         },
         {
